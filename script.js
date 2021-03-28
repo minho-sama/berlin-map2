26762a44095731444bffc99db9d2b046d87ecfa5
minho-sama <n.a.minh1106@gmail.com>
2021-03-29 01:59:16 +0200

    icons demo

---
+++ script.js
@@ -0,0 +1,9 @@
+function openFormBear() {
+    document.querySelector(".form-popup").style.display = "block";
+  }
+
+  function closeFormBear() {
+    document.querySelector(".form-popup").style.display = "none";
+  }
+
+  //ha advancebb leszek akkor az icon nyitás és zárás egyben --> if icon.style.display == hiddennel
