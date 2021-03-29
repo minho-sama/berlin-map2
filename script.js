1b036f9716ee7d89f097786d299988179d65a87b
minho-sama <n.a.minh1106@gmail.com>
2021-03-29 03:07:45 +0200

    styled popup, go to sleep

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ function openFormBear() {
   function closeFormBear() {
     document.querySelector(".form-popup").style.display = "none";
   }
+  //ne bonyolítsd túl, csak másold majd ki egymás után többször
 
   //ha advancebb leszek akkor az icon nyitás és zárás egyben --> if icon.style.display == hiddennel
